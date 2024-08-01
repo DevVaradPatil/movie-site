851586a522cc57140872ab8d5dcd9f0b9f8201e2
DevVaradPatil <varadapatil123@gmail.com>
2024-08-01 18:05:54 +0530

    Routing and color scheme added

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import Signup from './pages/Signup'
 function App() {
 
   return (
-<<<<<<< HEAD
    <Router>
       <Navbar/>
       <Routes>
@@ -17,9 +16,6 @@ function App() {
         <Route path="/signup" element={<Signup/>} />
       </Routes>
    </Router>
-=======
-
->>>>>>> ea28ec7d897356add7f9f4d3dd9beefa8f60d0ee
   )
 }
 
