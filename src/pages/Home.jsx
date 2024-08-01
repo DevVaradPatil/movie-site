35755df873dcd1c57ab45fbf7b3eeebf842cf25a
DevVaradPatil <varadapatil123@gmail.com>
2024-08-01 18:04:37 +0530

    Routing and color scheme added

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className=' bg-primary-bg h-screen text-primary-text'>
+      Home
+      <div className='w-full h-10 bg-secondary-bg text-secondary-text '>
+      see more
+      </div> 
+      <button className=' bg-button-primary-bg text-button-primary-text'>
+      Watch Now
+      </button>
+    </div>
+  )
+}
+
+export default Home
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,47 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          bg: '#0D0D0F',
+          text: '#FFFFFF',
+        },
+        secondary: {
+          bg: '#1A171E',
+          text: '#CCCCCC',
+        },
+        tertiary: {
+          bg: '#333333',
+        },
+        accent: {
+          primary: '#EB1C24',
+          secondary: '#821118',
+          tertiary: '#FF8888',
+        },
+        highlight: '#FFD700',
+        border: {
+          primary: '#444444',
+          secondary: '#555555',
+        },
+        divider: '#666666',
+        button: {
+          primary: {
+            bg: '#EB1C24',
+            text: '#FFFFFF',
+            hover: '#FF2A2A',
+          },
+          secondary: {
+            bg: '#FF6B6B',
+            text: '#FFFFFF',
+          },
+          disabled: {
+            bg: '#333333',
+            text: '#888888',
+          },
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
