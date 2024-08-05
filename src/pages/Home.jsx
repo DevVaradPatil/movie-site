b88f96db02aa7d3bc3a647e3252e00c75abf23f2
DevVaradPatil <varadapatil123@gmail.com>
2024-08-05 21:51:44 +0530

    tmdb api added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,48 @@
-import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { getNowPlayingMovies } from '../utils/api';
 
 const Home = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const moviesData = await getNowPlayingMovies();
+      setMovies(moviesData);
+    };
+
+    fetchMovies();
+  }, []);
+
+  useEffect(() => {
+    console.log(movies);
+    
+  }, [movies])
+  
+  
+
   return (
-    <div className=' bg-primary-bg h-screen text-primary-text'>
+    <div className='bg-primary-bg h-screen text-primary-text'>
       Home
       <div className='w-full h-10 bg-secondary-bg text-secondary-text '>
-      see more
-      </div> 
-      <button className=' bg-button-primary-bg text-button-primary-text'>
-      Watch Now
+        see more
+      </div>
+      <button className='bg-button-primary-bg text-button-primary-text'>
+        Watch Now
       </button>
       <NavLink to='signup' className='mt-5'>SignUp</NavLink>
+
+      <div className='movie-list'>
+        {movies && movies.map(movie => (
+          <div key={movie.id} className='py-2'>
+            <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt="" />
+            <h3>{movie.title}</h3>
+            <p>{movie.overview}</p>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
