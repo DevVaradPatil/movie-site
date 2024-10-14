1d11c4be948dd12d0a50b105fb468ecdf27b0453
DevVaradPatil <varadapatil123@gmail.com>
2024-10-14 10:39:59 +0530

    about updated

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import AboutCard from "../components/AboutCard";
 import img1 from "../assets/varad.jpg";
 import img2 from "../assets/saket.jpg";
-import img3 from "../assets/digvijay.jpg";
-import img4 from "../assets/prathmesh.jpg";
 
 const About = () => {
   const aboutCard = [
@@ -20,21 +18,7 @@ const About = () => {
       prn: "2103062",
       gradient: "bg-gradient-to-r from-yellow-800 via-orange-700 to-red-800",
       linkedin: "https://www.linkedin.com/in/saket-patil-060640228/",
-    },
-    {
-      img: img3,
-      name: "Digvijay Dawar",
-      prn: "2103068",
-      gradient: "bg-gradient-to-tr from-red-900 via-rose-900 to-purple-900",
-      linkedin: "https://www.linkedin.com/in/digvijay-dawar-6a6011239/",
-    },
-    {
-      img: img4,
-      name: "Prathmesh Thorat",
-      prn: "2103030",
-      gradient: "bg-gradient-to-b from-blue-900 via-cyan-900 to-teal-800",
-      linkedin: "https://www.linkedin.com/in/prathmeshrt/",
-    },
+    }
   ];
 
   return (
